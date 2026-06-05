e209bcae84e83681b9369a1327d5c24a78cbc617
debonnaiR3 <ashpreetsingh1916@gmail.com>
2026-06-06 00:36:42 +0530

    init

---
+++ assignment1/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {BrowserRouter,Routes,Route,Navigate} from "react-router-dom";
+import Login from "./pages/login";
+import Dashboard from './pages/dashboard';
+
+
+export default function App(){
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Navigate to='/login' replace/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
